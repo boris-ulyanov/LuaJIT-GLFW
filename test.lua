4d1dc844f0d7bfa43ef470b2e253f14e50fa1bae
Boris Ulyanov <boris-ulyanov@mail.ru>
2015-04-28 22:49:43 +0300

    Edit test for OpenGL ES

--- test.lua
+++ test.lua
@@ -1,75 +1,48 @@
+#!/usr/bin/luajit
 
 -- Load libraries
-local lj_glfw = require "glfw"
+local lj_glfw = require "glfw-gles"
 local ffi = require "ffi"
 local bit = require "bit"
 -- Localize the FFI libraries
-local gl, glc, glu, glfw, glext = lj_glfw.libraries()
+local gl, glc, glfw, glext = lj_glfw.libraries()
 
 assert(pcall(function() local _ = glc.this_doesnt_exist end) == false)
-
--- Define vertex arrays for the model we will draw
-local CubeVerticies = {}
-CubeVerticies.v = ffi.new("const float[8][3]", {
-	{0,0,1}, {0,0,0}, {0,1,0}, {0,1,1},
-	{1,0,1}, {1,0,0}, {1,1,0}, {1,1,1}
-})
-
-CubeVerticies.n = ffi.new("const float[6][3]", {
-	{-1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 0.0, 0.0},
-	{0.0, -1.0, 0.0}, {0.0, 0.0, -1.0}, {0.0, 0.0, 1.0}
-})
-
-CubeVerticies.f = ffi.new("const float[6][4]", { 
-	{0, 1, 2, 3}, {3, 2, 6, 7}, {7, 6, 5, 4},
-	{4, 5, 1, 0}, {5, 6, 2, 1}, {7, 4, 0, 3}
-})
 
 -- Initialize GLFW. Unline glfwInit, this throws an error on failure
 lj_glfw.init()
 
+-- select appropriate context API
+lj_glfw.hint(glc.GLFW_CLIENT_API, glc.GLFW_OPENGL_ES_API);
+lj_glfw.hint(glc.GLFW_CONTEXT_VERSION_MAJOR, 2);
+
 -- Creates a new window. lj_glfw.Window is a ctype object with a __new metamethod that
 -- runs glfwCreateWindow.
 -- The window ctype has most of the windows functions as methods
-local window = lj_glfw.Window(1024, 768, "LuaJIT-GLFW Test")
+local window = lj_glfw.Window(400, 300, "LuaJIT-GLFW Test")
+
+window:setKeyCallback(
+  function(win,key,scan,action,mods)
+    if (key==glc.GLFW_KEY_ESCAPE or key==glc.GLFW_KEY_Q) and action == glc.GLFW_PRESS then
+      win:setShouldClose(true)
+    end
+  end
+)
 
 -- Initialize the context. This needs to be called before any OpenGL calls.
 window:makeContextCurrent()
 
-local w, h = window:getFramebufferSize()
+local ver = ffi.string(gl.glGetString(glc.GL_VERSION))
+print("GL_VERSION = "..ver)
 
--- Set up OpenGL
-gl.glEnable(glc.GL_DEPTH_TEST);
-
-gl.glMatrixMode(glc.GL_PROJECTION)
-glu.gluPerspective(60, w/h, 0.01, 1000)
-gl.glMatrixMode(glc.GL_MODELVIEW)
-glu.gluLookAt(0,0,5,
-	0,0,0,
-	0,1,0)
-
--- Set up some values
-local rotx, roty, rotz = 1/math.sqrt(2), 1/math.sqrt(2), 0
-local boxx, boxy, boxz = -0.5,-0.5,2
-
+local c = 0
 -- Main loop
 while not window:shouldClose() do
-	gl.glClear(bit.bor(glc.GL_COLOR_BUFFER_BIT, glc.GL_DEPTH_BUFFER_BIT))
-	
-	gl.glPushMatrix()
-	gl.glColor3d(1,1,1)
-	gl.glTranslated(boxx, boxy, boxz)
-	gl.glRotated(lj_glfw.getTime()*10, rotx, roty, rotz)
-	for i=0,5 do
-		gl.glBegin(glc.GL_QUADS)
-		gl.glNormal3fv(CubeVerticies.n[i])
-		for j=0,3 do
-			gl.glVertex3fv(CubeVerticies.v[CubeVerticies.f[i][j]])
-		end
-		gl.glEnd()
-	end
-	gl.glPopMatrix()
-	
+	gl.glClearColor(c, c, c, 1.0)
+	gl.glClear(glc.GL_COLOR_BUFFER_BIT)
+	c = c + 1 / 200
+	if c > 1 then c = 0 end
+
 	window:swapBuffers()
 	lj_glfw.pollEvents()
 end
